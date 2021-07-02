609268b42ebd4ca1f4c5931b3cad3e1d1789dd4d
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-07-02 10:57:04 +0700

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,7 +1,5 @@
 from typing import Union, List
 
-from pyrogram import filters
-
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 
 def command(commands: Union[str, List[str]]):
